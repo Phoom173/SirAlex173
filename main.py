28098e79b1cd29693283d24bff051df3e31c76b4
Phoom173 <ggpanzar173@gmail.com>
2025-07-26 11:22:24 +0700

    up to global

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-from typing import List
+from typing import List, Dict, Optional
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
 # Temporary storage for todos (in-memory)
-todos: List[str] = []
+todos: List[Dict[str, str]] = []
+next_id: int = 1
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
@@ -16,5 +17,12 @@ def read_root(request: Request):
 
 @app.post("/create-todo")
 def create_todo(item: str = Form(...)):
-    todos.append(item)
+    global next_id
+    todo_item = {"id": str(next_id), "item": item, "owner": owner.lower()}
+    todos.append(todo_item)
+    next_id += 1
     return RedirectResponse("/", status_code=303)
+
+@app.put("/todos/{todo_id}")
+    request_owner = owner.lower()
+    
